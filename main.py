809c135ba24d66b4fea6fe8739062571fd57c7a1
rahmatu23 <rahmatu.abdullahiliman@edu.sait.ca>
2023-11-02 17:29:58 -0600

    Verified Exercise

---
+++ main.py
@@ -0,0 +1,25 @@
+f = open("three_digit_numbers.txt")
+sample = f.read()
+sample = "380 339 420 308 448"
+
+
+cols = sample.split()
+num_list =[]
+for col in cols:
+
+    num = int (col)
+    num_list.append(num)
+start = min(num_list)
+for count in range(start, max(num_list)):
+        #print(count)
+    # if count in num_list:
+        # we have membership
+            # pass
+# else:
+    #otherwise
+    if count not in num_list:
+        print(f"{count}, missing")
+
+
+
+
