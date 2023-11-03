7864ae08f8dfabc3874cc280a9ee3e65216d944f
rahmatu23 <rahmatu.abdullahiliman@edu.sait.ca>
2023-11-02 18:19:56 -0600

    Double Check

---
+++ 1.py
@@ -0,0 +1,22 @@
+# sort numbers numerically
+f = open("three_digit_numbers.txt")
+sample = f.read()
+numbers = (sample.split())
+
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
